420b49fdc2e9d442e5d1403c227532dd0ee76eb8
Sunny <123319227+sunnysh17@users.noreply.github.com>
2023-04-13 00:57:39 +0000

    created FirstPrgram.java

--- intro/FirstProgram.java
+++ intro/FirstProgram.java
@@ -1,1 +1,10 @@
+public class FirstProgram{  //class name should be same as file name.
+    public static void main(String[] args) { //main is starting point of program.
+        //In order to make the main methid acessible to JVM we need to declare public.
+        //static - If we want to use anything from the class without creating a object then that shuld be declared as static.
 
+        System.out.println("Hello World!");
+//      class  object method()
+        System.out.println(args[0]); //command line arguments.
+    }
+}
